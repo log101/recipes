18140451bf85fdb34f419bd93199d4cdfc6007e3
log101 <frknerdm@mail.com>
2022-11-29 12:21:09 +0300

    List recipes with cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,26 @@
 import 'package:flutter/material.dart';
+import 'recipe.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RecipeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'Recipe Calculator',
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.blueGrey,
+          secondary: Colors.black,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Recipe Calculator'),
     );
   }
 }
@@ -71,45 +67,26 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: SafeArea(
+        child: ListView.builder(
+            itemCount: Recipe.samples.length,
+            itemBuilder: (BuildContext context, int index) {
+              return buildRecipeCard(Recipe.samples[index]);
+            }),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          Text(recipe.label),
+        ],
+      )
     );
   }
 }
---
+++ lib/recipe.dart
@@ -0,0 +1,33 @@
+class Recipe {
+  String label;
+  String imageUrl;
+
+  Recipe(this.label, this.imageUrl);
+
+  static List<Recipe> samples = [
+    Recipe(
+      'Spaghetti and Meatballs',
+      'assets/2126711929_ef763de2b3_w.jpg',
+    ),
+    Recipe(
+      'Tomato Soup',
+      'assets/27729023535_a57606c1be.jpg',
+    ),
+    Recipe(
+      'Grilled Cheese',
+      'assets/3187380632_5056654a19_b.jpg',
+    ),
+    Recipe(
+      'Chocolate Chip Cookies',
+      'assets/15992102771_b92f4cc00a_b.jpg',
+    ),
+    Recipe(
+      'Taco Salad',
+      'assets/8533381643_a31a99e8a6_c.jpg',
+    ),
+    Recipe(
+      'Hawaiian Pizza',
+      'assets/15452035777_294cefced5_c.jpg',
+    ),
+  ];
+}
